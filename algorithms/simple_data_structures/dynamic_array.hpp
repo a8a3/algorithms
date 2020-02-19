8ad4e359d1768ee34d6dc3f2af973f83cba3987a
Alexey Abramov <a.abramov@egopay.ru>
2020-02-19 12:01:34 +0300

    single_array + tests impl

--- algorithms/simple_data_structures/dynamic_array.hpp
+++ algorithms/simple_data_structures/dynamic_array.hpp
@@ -6,14 +6,13 @@ class array {
 
 public:
     size_t size() const {return size_;}
+    const T& operator[](size_t idx) const { return arr_[idx]; }
 
-    virtual void add   (const T& item, size_t index) = 0;
-    virtual T    remove(size_t index)                = 0;
-    virtual ~array() {
-        delete []arr_;
-    }
-private:
-    virtual void resize(size_t size) = 0;
+    virtual void add_back(const T& item)             = 0;
+    virtual void add     (const T& item, size_t idx) = 0;
+    virtual T    remove  (size_t idx)                = 0;
+
+    virtual ~array() {delete []arr_;}
 
 protected:
     T* arr_{ nullptr };
@@ -21,29 +20,96 @@ protected:
 };
 
 // ------------------------------------------------------------------
+template <typename T>
+void copy_range(const T* src, T* dst, size_t size) {
+   for (size_t i = 0; i < size; ++i) {
+      *(dst + i) = *(src + i);
+   }
+}
+
+// ------------------------------------------------------------------
 template<typename T>
 class single_array : public array<T> {
 public:
-    void add(const T& item, size_t index) override {
-        if (index < size_) {
-            
-        } else {
 
-        }
+   void add_back(const T& item) override {
+      add(item, array<T>::size_);
+   }
+
+   void add(const T& item, size_t idx) override {
+      auto& sz = array<T>::size_;
+      if (idx > sz) {
+         throw std::out_of_range("incorrect index requested");
+      }
+      auto& arr = array<T>::arr_;
+
+      T* buf = new T[sz + 1];
+      buf[idx] = item;
+
+      if (idx == 0) { // add to front
+         copy_range(arr, buf + 1, sz);
+      } else if (idx == sz) { // add to back
+         copy_range(arr, buf, sz);
+      } else { // add in middle
+         copy_range(arr, buf, idx);
+         copy_range(arr+idx, buf+idx+1, sz-idx);
+      }
+      delete []arr;
+      arr = buf;
+      ++sz;
     }
 
-    T remove(size_t index) override {
-        if (index >= size_) return T();
-        
-        return T();
-    }
+    T remove(size_t idx) override {
+       auto& sz = array<T>::size_;
 
-private:
-    void resize(size_t size) override {
-        delete []arr_;
-        arr_ = new T[size];
-        size_ = size;
+       if (idx >= sz) {
+          throw std::out_of_range("incorrect index requested");
+       }
+
+       auto& arr = array<T>::arr_;
+
+       T result{arr[idx]};
+       T* buf = new T[sz - 1];
+
+       copy_range(arr, buf, idx);
+       copy_range(arr+idx+1, buf+idx, sz-(idx+1));
+
+       delete []arr;
+       arr = buf;
+       --sz;
+       return result;
     }
+};
+
+// ------------------------------------------------------------------
+template <typename T>
+class vector_array : public array<T> {
+   void add_back(const T& item) override {
+      add(item, array<T>::size_);
+   }
+
+   void add(const T&, size_t idx) override {
+      auto& sz = array<T>::size_;
+      if (idx > sz) {
+         throw std::out_of_range("incorrect index requested");
+      }
+      auto& arr = array<T>::arr_;
+      ++sz;
+   }
+
+   T remove(size_t idx) override {
+      auto& sz = array<T>::size_;
+
+      if (idx >= sz) {
+         throw std::out_of_range("incorrect index requested");
+      }
+
+      auto& arr = array<T>::arr_;
+
+      T result{arr[idx]};
+      --sz;
+      return result;
+   }
 };
 
 // TODO
--- tests/simple_data_structures.cpp
+++ tests/simple_data_structures.cpp
@@ -1,9 +1,88 @@
 #define CATCH_CONFIG_MAIN   // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 
+#include <iostream>
+#include <vector>
+
 #include <dynamic_array.hpp>
 
+template<typename T>
+void print(const array<T>& array) {
+   for (size_t i = 0, sz = array.size(); i < sz; ++i) {
+      std::cerr << array[i] << ' ';
+   }
+   std::cerr << '\n';
+}
+
+template <typename T>
+std::vector<T> as_vector(const array<T>& array){
+   std::vector<T> result(array.size());
+   for (size_t i = 0, sz = array.size(); i < sz; ++i) {
+      result[i] = array[i];
+   }
+   return result;
+}
+
 // ------------------------------------------------------------------
-TEST_CASE("test1", "[dynamic_array]") {
-   CHECK(26 == 26);
+TEST_CASE("dynamic_array_test", "[dynamic_array]") {
+   single_array<int> array;
+   CHECK(array.size() == 0);
+
+   SECTION("add values to back and delete it from back") {
+      array.add_back(0);
+      array.add_back(1);
+      array.add_back(2);
+      array.add_back(3);
+      array.add_back(4);
+      CHECK(array.size() == 5);
+      CHECK(as_vector<int>(array) == std::vector<int>{0, 1, 2, 3, 4});
+
+      CHECK_THROWS_WITH(array.remove(array.size()), "incorrect index requested");
+
+      CHECK(array.remove(array.size() - 1) == 4);
+      CHECK(array.remove(array.size() - 1) == 3);
+      CHECK(array.remove(array.size() - 1) == 2);
+      CHECK(array.remove(array.size() - 1) == 1);
+      CHECK(array.remove(array.size() - 1) == 0);
+
+      CHECK(array.size() == 0);
+   }
+
+   SECTION("add values to front and delete it from front") {
+      array.add(0, 0);
+      array.add(1, 0);
+      array.add(2, 0);
+      array.add(3, 0);
+      array.add(4, 0);
+      CHECK(array.size() == 5);
+      CHECK(as_vector<int>(array) == std::vector<int>{4, 3, 2, 1, 0});
+
+      CHECK(array.remove(0) == 4);
+      CHECK(array.remove(0) == 3);
+      CHECK(array.remove(0) == 2);
+      CHECK(array.remove(0) == 1);
+      CHECK(array.remove(0) == 0);
+
+      CHECK(array.size() == 0);
+   }
+
+   SECTION("add values in middle and delete it from middle") {
+      array.add(10, 0);
+      array.add(20, 1);
+      array.add(30, 2);
+      CHECK(array.size() == 3);
+
+      CHECK_THROWS_WITH(array.add(50, 5), "incorrect index requested");
+      CHECK(array.size() == 3);
+
+      array.add(42, 1);
+      CHECK(as_vector<int>(array) == std::vector<int>{10, 42, 20, 30});
+      CHECK(array.remove(1) == 42);
+      CHECK(as_vector<int>(array) == std::vector<int>{10, 20, 30});
+      CHECK(array.remove(1) == 20);
+      CHECK(array.remove(1) == 30);
+      CHECK(array.remove(0) == 10);
+
+      CHECK(array.size() == 0);
+   }
 }
